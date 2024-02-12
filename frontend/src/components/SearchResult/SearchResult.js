7466cba1e92fb399457eba834dd4db50c18f1b07
Prince Kumar <prince2520p@gmail.com>
2024-02-12 19:48:42 +0530

    changes in search result anime manga

--- frontend/src/components/SearchResult/SearchResult.js
+++ frontend/src/components/SearchResult/SearchResult.js
@@ -1,62 +1,88 @@
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-import randomColor from 'randomcolor'
+import randomColor from "randomcolor";
 
-import './SearchResult.css';
+import "./SearchResult.css";
 
-const SearchResult = (props) => {
-    const navigate = useNavigate();
+const SearchResult = ({ results, closeSearchHandler }) => {
+  const navigate = useNavigate();
 
-    return (
-        <div className='search-result-container'>
-            {props.results.map(res => {
-                    let ratingCircleColor = randomColor({
-                        luminosity: 'dark',
-                        hue: 'random'
+  return (
+    <div className="shadow search-result-box">
+      {results.map((res) => {
+        let ratingCircleColor = randomColor({
+          luminosity: "dark",
+          hue: "random",
+        });
+        return (
+          <div
+            key={res.toString()}
+            className="search-result-item"
+            onClick={() => navigate(`anime/${res.node.id}`)}
+          >
+            <div className="flex-center search-result-item-left">
+              <div className="search-result-img-container">
+                <img alt="search-result" src={res.node.main_picture.large} />
+              </div>
+            </div>
+
+            <div className="search-result-item-middle">
+              <h5 className="color-text">{res.node.title}</h5>
+              <ul className="search-result-detail">
+                {[
+                  res.node.media_type.toUpperCase(),
+                  res.node.status,
+                  res.node.start_date,
+                ].map((data) => (
+                  <li key={data.toString()}>
+                    <h6>{data.toUpperCase()}</h6>
+                  </li>
+                ))}
+              </ul>
+              <div className="genres">
+                <ul>
+                  {res.node.genres.map((g) => {
+                    let color = randomColor({
+                      luminosity: "light",
+                      hue: "random",
                     });
-                    return <div key={res.toString()} className="search-result-item"
-                                onClick={() => navigate(`anime/${res.node.id}`)}>
-                        <div className="search-result-item-left">
-                            <div className="search-result-img-container">
-                                <img alt='search-result' src={res.node.main_picture.large}/>
-                            </div>
-                        </div>
-                        <div className="search-result-item-middle">
-                            <span style={{fontSize: '1rem', fontWeight: '600'}}>{res.node.title}</span>
-                            <ul className="search-result-detail">
-                                {[res.node.media_type.toUpperCase(), res.node.status, res.node.start_date]
-                                    .map(data => <li key={data.toString()}>{res.node.media_type.toUpperCase()}</li>)}
-                            </ul>
-                            <div className='genres'>
-                                <ul>
-                                    {
-                                        res.node.genres.map(g => {
-                                            let color = randomColor({
-                                                luminosity: 'light',
-                                                hue: 'random'
-                                            });
-                                            return <li
-                                                key={g.toString()}
-                                                className="genre-list"
-                                                style={{border: `2px ${color} solid`, color: `${color}`}}
-                                                id={g.id}>{g.name}</li>
-                                        })
-                                    }
-                                </ul>
-                            </div>
-                        </div>
-                        <div className='search-result-item-right'>
-                            <div className='rating' style={{border: `0.25rem ${ratingCircleColor} solid`}}>
-                                {res.node.mean}
-                            </div>
-                        </div>
-                    </div>
-                }
-            )}
-            <div className="view-all" onClick={() => props.closeSearchHandler()}><span>Close</span></div>
-        </div>
+                    return (
+                      <li
+                        key={g.toString()}
+                        className="genre-list"
+                        style={{
+                          border: `1px ${color} solid`,
+                        }}
+                        id={g.id}
+                      >
+                        <h6 style={{ color: `${color}` }}>{g.name}</h6>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </div>
 
-    );
-}
+            <div className="search-result-item-right">
+              <div
+                className="flex-center rating"
+                style={{ border: `0.25rem ${ratingCircleColor} solid` }}
+              >
+                <p className="color-text">{res.node.mean}</p>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+
+      <div
+        className="flex-center cursor-btn view-all"
+        onClick={() => closeSearchHandler()}
+      >
+        <p className="color-text">Close</p>
+      </div>
+    </div>
+  );
+};
 
 export default SearchResult;
