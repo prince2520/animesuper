d0e1c706686b090a3c34d6e59c0f0b59efb92085
Prince Kumar <prince2520p@gmail.com>
2024-02-15 11:07:24 +0530

    added repsonsive font for all resolution

--- frontend/src/components/Overlay/overlay.js
+++ frontend/src/components/Overlay/overlay.js
@@ -1,43 +1,44 @@
-import {motion} from "framer-motion";
-import {useDispatch, useSelector} from "react-redux";
+import { motion } from "framer-motion";
+import { useDispatch, useSelector } from "react-redux";
 
 import Modal from "../Modal/Modal";
 import ProfileCard from "../ProfileCard/ProfileCard";
 
-import {OverlayActions} from "../../store/overlay";
-import {MyWatchlistActions} from "../../store/myWatchlist";
+import { OverlayActions } from "../../store/overlay";
+import { MyWatchlistActions } from "../../store/myWatchlist";
 
-import './overlay.css';
+import "./overlay.css";
 
 const Overlay = () => {
-    const dispatch = useDispatch();
-    const showProfile = useSelector(state => state.overlay.showProfile);
+  const dispatch = useDispatch();
+  const showProfile = useSelector((state) => state.overlay.showProfile);
 
-    const overlayVariant = {
-        initial: {
-            opacity: 0
-        },
-        animate: {
-            opacity: 1,
-        }
-    }
+  const overlayVariant = {
+    initial: {
+      opacity: 0,
+    },
+    animate: {
+      opacity: 1,
+    },
+  };
 
-    return (
-        <div className="flex-center overlay-page">
-            <motion.div
-                variants={overlayVariant}
-                initial="initial"
-                animate="animate"
-                transition={{duration: 0.15}}
-                className="overlay-box" onClick={() => {
-                dispatch(OverlayActions.closeOverlayHandler())
-                dispatch(MyWatchlistActions.selectedWatchlistItemHandler({}))
-            }}/>
-            {showProfile && <ProfileCard/>}
-            {!showProfile && <Modal/>}
-        </div>
-    );
-
-}
+  return (
+    <div className="flex-center overlay-page">
+      <motion.div
+        variants={overlayVariant}
+        initial="initial"
+        animate="animate"
+        transition={{ duration: 0.15 }}
+        className="overlay-box"
+        onClick={() => {
+          dispatch(OverlayActions.closeOverlayHandler());
+          dispatch(MyWatchlistActions.selectedWatchlistItemHandler({}));
+        }}
+      />
+      {showProfile && <ProfileCard />}
+      {!showProfile && <Modal />}
+    </div>
+  );
+};
 
 export default Overlay;
