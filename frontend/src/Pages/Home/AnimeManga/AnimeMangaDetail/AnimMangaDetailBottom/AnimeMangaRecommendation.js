b23ebb6a04f47377ad17c6a24977a20014428227
Prince Kumar <prince2520p@gmail.com>
2024-04-07 18:35:46 +0530

    style: border color

--- frontend/src/Pages/Home/AnimeManga/AnimeMangaDetail/AnimMangaDetailBottom/AnimeMangaRecommendation.js
+++ frontend/src/Pages/Home/AnimeManga/AnimeMangaDetail/AnimMangaDetailBottom/AnimeMangaRecommendation.js
@@ -2,19 +2,23 @@ import React from "react";
 
 import Card from "../../../../../components/Card/Card";
 
-const AnimeMangaRecommendation = ({animeDetail}) => {
-    return (
-        <div className="recommendation">
-            <h3>Recommendation for you</h3>
-            <div className="flex-center recommendation-container">
-                {animeDetail?.recommendations.length > 0 ?
-                    animeDetail.recommendations.map(res =>
-                        <div className={'card-container'}>
-                            <Card isRecommemdation={true} detail={res.node}/>
-                        </div>) : <p style={{width:'100%'}}>No recommendations found!</p>}
+const AnimeMangaRecommendation = ({ animeDetail }) => {
+  return (
+    <div className="recommendation">
+      <h3>Recommendation for you</h3>
+      <div className="flex-center recommendation-container">
+        {animeDetail?.recommendations.length > 0 ? (
+          animeDetail.recommendations.map((res) => (
+            <div className={"card-container"}>
+              <Card isRecommemdation={true} detail={res.node} />
             </div>
-        </div>
-    );
+          ))
+        ) : (
+          <p style={{ width: "100%" }}>No recommendations found!</p>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default React.memo(AnimeMangaRecommendation);
